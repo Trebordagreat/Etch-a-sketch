5f7c56681dbeb5a5f96dfc95400b80ec46e0c97f
Tye Rooth <tyerooth@gmail.com>
2021-11-20 18:34:15 -0500

    created grid, next is align and format grid

--- script.js
+++ script.js
@@ -1,16 +1,21 @@
 let dimensions = 16;
 const main = document.querySelector('.main');
 
-// Generate the divs in one row.  There should be 'dimensions' amount of divs
-const row = document.createElement('div');
-row.classList.add('row');
-main.appendChild(row);
 
+//Generate number of rows equal to dimensions
 for (i = 0; i < dimensions; i++) {
-    const column = document.createElement('div');
-    column.classList.add('column');
-    row.appendChild(column);
+    const row = document.createElement('div');
+    row.classList.add('row');
+    main.appendChild(row);
+    
+    // Generate the divs in one row.  There should be 'dimensions' amount of divs
+    for (j = 0; j < dimensions; j++) {
+        const column = document.createElement('div');
+        column.classList.add('column');
+        row.appendChild(column);
+    }
 }
+
 
 
 
