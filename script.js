34bfaa23570c77a1dc5361edef79113dd418482a
Tye Rooth <tyerooth@gmail.com>
2021-11-20 19:07:26 -0500

    formatted grid, now create hover event

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 let dimensions = 16;
-const main = document.querySelector('.main');
+const grid = document.querySelector('.grid');
 
 
 //Generate number of rows equal to dimensions
 for (i = 0; i < dimensions; i++) {
     const row = document.createElement('div');
     row.classList.add('row');
-    main.appendChild(row);
-    
+    grid.appendChild(row);
+
     // Generate the divs in one row.  There should be 'dimensions' amount of divs
     for (j = 0; j < dimensions; j++) {
         const column = document.createElement('div');
