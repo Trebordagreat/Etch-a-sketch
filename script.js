4aa37ed6ee985440b3e6529794b19eb1cf0f05df
Tye Rooth <tyerooth@gmail.com>
2021-11-20 22:05:22 -0500

    change dimensions on grid works, finished project

--- script.js
+++ script.js
@@ -1,24 +1,37 @@
 let dimensions = 16;
-const grid = document.querySelector('.grid');
 
+const main = document.querySelector('.main');
 
-//Generate number of rows equal to dimensions
-for (i = 0; i < dimensions; i++) {
-    const row = document.createElement('div');
-    row.classList.add('row');
-    grid.appendChild(row);
+createGrid();
 
-    // Generate the divs in one row.  There should be 'dimensions' amount of divs
-    for (j = 0; j < dimensions; j++) {
-        const column = document.createElement('div');
-        column.classList.add('column');
-        row.appendChild(column);
+// Clear board when button is clicked
+const reset = document.querySelector('.reset');
+reset.addEventListener('click', clearGrid);
+
+// Prompt user for dimension change when button is clicked
+const newSize = document.querySelector('.dimensions');
+newSize.addEventListener('click', changeDimensions);
+
+function createGrid () {
+    const grid = document.createElement('div');
+    grid.classList.add('grid');
+    for (i = 0; i < dimensions; i++) {
+        const row = document.createElement('div');
+        row.classList.add('row');
+        grid.appendChild(row);
+    
+        // Generate the divs in one row.  There should be 'dimensions' amount of divs
+        for (j = 0; j < dimensions; j++) {
+            const column = document.createElement('div');
+            column.classList.add('column');
+            row.appendChild(column);
+        }
     }
+    main.appendChild(grid);
+    //Event listener for hovering over a square
+    const pixels = document.querySelectorAll('.column');
+    pixels.forEach(pixel => pixel.addEventListener('mouseenter', draw));
 }
-
-//Event listener for hovering over a square
-const pixels = document.querySelectorAll('.column');
-pixels.forEach(pixel => pixel.addEventListener('mouseenter', draw));
 
 // Event to turn pixel black
 function draw (e) {
@@ -26,14 +39,29 @@ function draw (e) {
     e.target.classList.add('drawn');
 } 
 
-// Clear board when button is clicked
-const reset = document.querySelector('.reset');
-reset.addEventListener('click', clearGrid);
-
 // Event for reset
 function clearGrid() {
-    console.log("Success")
     const drawns = document.querySelectorAll('.drawn');
     drawns.forEach(drawn => drawn.classList.remove('drawn'));
     drawns.forEach(drawn => drawn.classList.add('column'));
+}
+
+// Event for prompting user and changing dimensions
+function changeDimensions() {
+    clearGrid();
+    squares = Number(prompt("How many squares would you like per side?"));
+    if (squares === NaN) {
+        alert("Type valid integer between 1 - 100");
+        return 0;
+    }
+    else if (squares < 1 || squares > 100 || squares % 1 !== 0) {
+        alert("Use an integer between 1-100");
+    }
+    else {
+        dimensions = squares;
+        const grid = document.querySelector('.grid');
+        main.removeChild(grid);
+        createGrid();
+    }
+
 }
